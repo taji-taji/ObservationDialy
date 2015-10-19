3ddcbad5f339ff68db48b4ce787c3e4434549be6
Yutaka Tajika <y.tajika529@gmail.com>
2015-10-20 07:52:04 +0900

    Initial Commit

---
+++ PhotoLogger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoLogger
+//
+//  Created by tajika on 2015/10/20.
+//  Copyright © 2015年 Tajika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
