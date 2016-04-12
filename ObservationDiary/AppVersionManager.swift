628bf82badacdf267e7bd461aa09eb2708f12b50
Yutaka Tajika <y.tajika529@gmail.com>
2016-04-12 09:00:59 +0900

    バージョンアップ時に何かする（修正）

--- ObservationDiary/AppVersionManager.swift
+++ ObservationDiary/AppVersionManager.swift
@@ -30,9 +30,9 @@ struct AppVersionManager {
     static func compareVersion(target: String, greaterThan: String) -> Bool {
         switch target.compare(greaterThan, options: .NumericSearch, range: nil, locale: nil) {
         case .OrderedAscending:
-            return false
-        case .OrderedDescending:
             return true
+        case .OrderedDescending:
+            return false
         case .OrderedSame:
             return false
         }
@@ -42,7 +42,7 @@ struct AppVersionManager {
      バージョンアップ時処理
      */
     static func versionUp(target: UIViewController) {
-        if loadedVersion < version {
+        if compareVersion(loadedVersion, greaterThan: version) {
 
             versionUp_1_2_5(target)
             // 以降、バージョンアップに何かする時にはここに追加する
@@ -56,12 +56,12 @@ struct AppVersionManager {
      */
     static func versionUp_1_2_5(target: UIViewController) {
         let defaults = NSUserDefaults.standardUserDefaults()
-        let value = defaults.objectForKey(Tour.TourType.AddTarget.rawValue) as? Bool
+        let tourEnded = defaults.objectForKey(Tour.TourType.AddTarget.rawValue) as? Bool
         // このバージョンのみ、loadedVersionが全ユーザ "0.0.0" になり、新規登録ユーザも該当してしまう
         // そのため、初回ツアーが終わっているかでアップデートユーザかを判断している
         // 今後は、 loadedVersion != "0.0.0" && compareVersion(loadedVersion, greaterThan: "1.2.5")
         // でアップデートユーザを判断する
-        if compareVersion(loadedVersion, greaterThan: "1.2.5") && value == true {
+        if compareVersion(loadedVersion, greaterThan: "1.2.5") && tourEnded == true {
             let title = "新機能追加！！"
             let message = "記録ごとにムービーの再生スピードの設定が可能になりました！\n「記録リスト」の歯車アイコンから設定ができます。ぜひ、お好みのスピードで成長ムービーをお楽しみください。"
             let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
